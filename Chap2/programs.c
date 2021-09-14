fec3a2bc86cc915d2eca44b3ec5b1adb7fd8146e
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-09-14 23:53:20 +0530

    two questions solved.

--- Chap2/programs.c
+++ Chap2/programs.c
@@ -5,23 +5,121 @@
 // (a) If a five-digit number is input through the keyboard, write a
 // program to calculate the sum of its digits. (Hint: Use the modulus
 // operator ‘%’)
+// #include<stdio.h>
+// int main(){
+//     int num, sumOfDigits=0,i,digit;
+//     printf("Enter a five digit number: \n");
+//     scanf("%d",&num);
+//     for (i=1;i<=5;i++){
+//         digit = num%10;
+//         num=num/10;
+//         sumOfDigits=sumOfDigits+digit;
+//     }
+//     printf("the sum of digit is : %d", sumOfDigits);
+//     return 0;
+// }
+
+//or
+
+// #include<stdio.h>
+// int main(){
+//     int num, sumOfDigits,d1,d2,d3,d4,d5;
+//     printf("Enter a five digit number: \n");
+//     scanf("%d",&num);
+//         d1 = num%10;
+//         num=num/10;
+//          d2 = num%10;
+//         num=num/10;
+//          d3 = num%10;
+//         num=num/10;
+//          d4 = num%10;
+//         num=num/10;
+//          d5 = num%10;
+//         num=num/10;
+
+//        sumOfDigits=d1+d2+d3+d4+d5;
+//     printf("the sum of digit is : %d", sumOfDigits);
+//     return 0;
+// }
+
+
 // (b) If a five-digit number is input through the keyboard, write a
 // program to reverse the number.
+// #include<stdio.h>
+// int main(){
+//     int num, revNum,d1,d2,d3,d4,d5;
+//     printf("Enter a five digit number: \n");
+//     scanf("%d",&num);
+//         d5 = num%10;
+//         num=num/10;
+//          d4 = num%10;
+//         num=num/10;
+//          d3 = num%10;
+//         num=num/10;
+//          d2 = num%10;
+//         num=num/10;
+//          d1 = num%10;
+//         num=num/10;
+
+//       revNum= d5*10000+d4*1000+d3*100+d2*10+d1;
+//     printf("the reversed num is : %d", revNum);
+//     return 0;
+// }
+
+
 // (c) If lengths of three sides of a triangle are input through the
 // keyboard, write a program to find the area of the triangle.
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     float a,b,c,semiP,area;
+//     printf("Enter the sides of triangle: \n");
+//     scanf("%f %f %f",&a,&b,&c);
+//     semiP=(a+b+c)/2;
+//     area=sqrt(semiP*(semiP-a)*(semiP-b)*(semiP-c));
+//     printf("the area of trianlge is : %f", area);
+//     return 0;
+// }
+
+
 // (d) Write a program to receive Cartesian co-ordinates (x, y) of a point
-// and convert them into polar co-ordinates (r, ).
-// Hint: r = sqrt ( x2
-// + y2
-// ) and tan-1
-// ( y / x )
+// and convert them into polar co-ordinates (r,s ).
+// Hint: r = sqrt ( x2+ y2) and s=tan-1( y / x )
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     int x,y,r,s;
+//     printf("Enter the cartesian coordinates(in degrees): \n");
+//     scanf("%d %d",&x,&y);
+//     x=x*(3.14/180);
+//     y=y*(3.14/180);
+//     r=sqrt(pow(2,x)+pow(2,x));
+//     s=atan(y/x);
+//     printf("The value of polar coordinates is : %d %d", r,s);
+// }
+
+
 // (e) Write a program to receive values of latitude (L1, L2) and longitude
 // (G1, G2), in degrees, of two places on the earth and output the
 // distance (D) between them in nautical miles. The formula for
 // distance in nautical miles is:
-// D = 3963 cos
-// -1
-// ( sin L1 sin L2 + cos L1 cos L2 * cos ( G2 – G1 ) )
+// D = 3963 cos-1( sin L1 sin L2 + cos L1 cos L2 * cos ( G2 – G1 ) )
+// #include<stdio.h>
+// #include<math.h>
+// int main()
+// {
+//     float L1,L2,G1,G2,D;
+//     printf("Enter tha values of latitudes: \n");
+//     scanf("%f %f", &L1,&L2);
+//     printf("Enter the values of longitudes: \n");
+//     scanf("%f %f",&G1,&G2);
+//     D= 3963* acos((sin(L1)*sin(L2))+((cos(L1)*cos(L2))*cos(G2-G1)));
+//     printf("The distance in nautical miles is : %f",D);
+//     return 0;
+// }
+
+
+
 // (f) Wind chill factor is the felt air temperature on exposed skin due to
 // wind. The wind chill temperature is always lower than the air
 // temperature, and is calculated as per the following formula:
