3ce8315fb98e514b6c82ea9fb7af442d6d8ca549
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-09-16 22:31:46 +0530

    questions solved

--- Chap2/programs.c
+++ Chap2/programs.c
@@ -123,17 +123,101 @@
 // (f) Wind chill factor is the felt air temperature on exposed skin due to
 // wind. The wind chill temperature is always lower than the air
 // temperature, and is calculated as per the following formula:
-// wcf = 35.74 + 0.6215t + ( 0.4275t - 35.75 ) * v
-// 0.16
+// wcf = 35.74 + 0.6215t + ( 0.4275t - 35.75 ) * v0.16
 // where t is the temperature and v is the wind velocity. Write a
 // program to receive values of t and v and calculate wind chill factor
 // (wcf).
+
+
+
+
 // (g) If value of an angle is input through the keyboard, write a program
 // to print all its Trigonometric ratios.
+// #include<stdio.h>
+// #include<math.h>
+// int main(){
+//     float theta;
+//     float sinv,cosv,tanv,cosecv,secv,cotv;
+//     printf("Enter the angle: \n");
+//     scanf("%d", &theta);
+//     theta= (3.14*theta)/180;
+//     sinv= sin(theta);
+//     cosv= cos(theta);
+//     tanv= tan(theta);
+//     cosecv= 1/sinv;
+//     secv= 1/cosv;
+//     cotv= 1/tanv;
+//     printf("sin = %f \n", sinv);
+//     printf("cos = %f \n", cosv);
+//     printf("tan = %f \n", tanv);
+//     printf("cosec = %f \n", cosecv);
+//     printf("sec = %f \n", secv);
+//     printf("cot = %f \n", cotv);
+//   return 0;
+// }
+
+
+
 // (h) Two numbers are input through the keyboard into two locations C
 // and D. Write a program to interchange the contents of C and D.
+// #include<stdio.h>
+// int main(){
+//     int a,b,c;
+//     printf("Enter the values of a and b: \n");
+//     scanf("%d %d", &a,&b);
+//     printf("original values of a= %d and b= %d \n", a,b);
+//     c=a;
+//     a=b;
+//     b=c;
+//     printf("new vlaues of a= %d and b= %d", a, b);
+//     return 0;
+// }
+
+//or
+
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter the values of a and b: \n");
+//     scanf("%d %d", &a,&b);
+//     printf("original values of a= %d and b= %d \n", a,b);
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+//     printf("new vlaues of a= %d and b= %d", a, b);
+//     return 0;
+// }
+
+
+
 // (i) Consider a currency system in which there are notes of seven
 // denominations, namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If
 // a sum of Rs. N is entered through the keyboard, write a program to
 // compute the smallest number of notes that will combine to give Rs.
 // N
+
+// #include<stdio.h>
+// int main(){
+//     int money,n100,n50,n10,n5,n2,n1;
+//     printf("Enter the sum of money: \n");
+//     scanf("%d",&money);
+//     n100= money/100;
+//     money=money%100;
+//     n50=money/50;
+//     money=money%50;
+//     n10=money/10;
+//     money=money%10;
+//     n5=money/5;
+//     money=money%5;
+//     n2=money/2;
+//     money=money%2;
+//     n1=money/1;
+//     money=money%1;
+//     printf("there are notes of 100= %d \n",n100);
+//     printf("there are notes of 50= %d \n",n50);
+//     printf("there are notes of 10= %d \n",n10);
+//     printf("there are notes of 5= %d \n",n5);
+//     printf("there are notes of 2= %d \n",n2);
+//     printf("there are notes of 1= %d \n",n1);
+//     return 0;
+// }
